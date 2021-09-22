94683600013f7d703e660ce0f7d952564ac348ee
IsratMz <88873564+IsratMz@users.noreply.github.com>
2021-09-22 09:27:10 +0600

    Add files via upload

---
+++ Food_Order_System/View/css/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 Developed By - <a href="#"> Israt Mahzabeen</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
